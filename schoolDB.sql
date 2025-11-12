5d9b1c5e1cdbe607cb28f39336e6c8c019813d93
svel18122-star <svel18122@gmail.com>
2025-11-12 05:47:01 +0530

    Add files via upload

---
+++ schoolDB.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,  -- unique ID for each student
+    name VARCHAR(50),                   -- student name
+    age INT,                            -- student age
+    grade VARCHAR(10)                   -- grade or class (ex: 'A', '10th', etc.)
+);
+
+
+
